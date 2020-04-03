6f709d081be64e976a7a20444d7aea92456dd0fd
David Oort Alonso <davidoort@hotmail.com>
2020-04-03 14:56:40 +0200

    functional dicee app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,56 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: FlatButton(
+                padding: EdgeInsets.zero,
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                  print('Left button pressed!');
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: FlatButton(
+                padding: EdgeInsets.zero,
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                  print('Right button pressed!');
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
