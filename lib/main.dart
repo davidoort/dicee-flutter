4da5fe6a87953e710505c9bdee4d7b5a4894c761
David Oort Alonso <davidoort@hotmail.com>
2020-04-03 15:13:27 +0200

    cleanup code with functions

--- lib/main.dart
+++ lib/main.dart
@@ -25,47 +25,36 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void _onDicePress(String diceSide) {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+    print('$diceSide button pressed!');
+  }
+
+  Widget clickableDie(String dieSide) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: FlatButton(
+          padding: EdgeInsets.zero,
+          child: dieSide == 'Right' ? Image.asset('images/dice$rightDiceNumber.png') : Image.asset('images/dice$leftDiceNumber.png'),
+          onPressed: () => _onDicePress(dieSide),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(15),
-              child: FlatButton(
-                padding: EdgeInsets.zero,
-                child: Image.asset('images/dice$leftDiceNumber.png'),
-                onPressed: () {
-                  setState(() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  });
-                  print('Left button pressed!');
-                },
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(15),
-              child: FlatButton(
-                padding: EdgeInsets.zero,
-                child: Image.asset('images/dice$rightDiceNumber.png'),
-                onPressed: () {
-                  setState(() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  });
-                  print('Right button pressed!');
-                },
-              ),
-            ),
-          ),
+          clickableDie('Left'),
+          clickableDie('Right')
         ],
       ),
     );
   }
 }
-
